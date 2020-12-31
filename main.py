a823b65edf190fb84bb8862fb51ce5ecdf313a04
rajeshnairnca <rajeshnairnca@gmail.com>
2020-12-31 17:04:54 +0000

    Gets the indicators data for all exchanges and countries

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ from tradingview_ta import TA_Handler, Interval
 
 app = Flask(__name__)
 
+# Interval.INTERVAL_1_WEEK
 
-@app.route('/<stock>/<exchange>/<country>')
-def indicators_data(stock, exchange, country):
+@app.route('/<stock>/<exchange>/<country>/<interval>')
+def indicators_data(stock, exchange, country, interval):
     handler = TA_Handler()
     handler.set_symbol_as(stock)
     handler.set_exchange_as_crypto_or_stock(exchange)
     handler.set_screener_as_stock(country)
-    handler.set_interval_as(Interval.INTERVAL_1_WEEK)
+    handler.set_interval_as(interval)
     analysis_one = handler.get_analysis()
     all_indicators = analysis_one.indicators
     json_file = {'query': all_indicators}
